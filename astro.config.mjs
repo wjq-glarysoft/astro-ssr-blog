61e9ad1debe68b77f019197987cb8d0688fbcf34
wjq <1003056522@qq.com>
2026-07-01 15:21:53 +0800

    修改astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,4 @@
-import { defineConfig, envField } from "astro/config";
-//import cloudflare from "@astrojs/cloudflare"; // 导入适配器
-
+import { defineConfig } from "astro/config";
 export default defineConfig({
   output: "static", // 必须改为 "server" 或 "hybrid"
   build: {
@@ -8,14 +6,6 @@ export default defineConfig({
     minify: true,
     // 如果使用服务端渲染，确保只包含必要的代码
   },
-  /* adapter: cloudflare({
-    mode: 'directory', // 改为 directory 模式
-  }), */
   site: "https://astro-ssr-blog.1003056522.workers.dev",
- /*  env: {
-    schema: {
-      OAUTH_GITHUB_CLIENT_ID: envField.string({ context: "server", access: "secret" }),
-      OAUTH_GITHUB_CLIENT_SECRET: envField.string({ context: "server", access: "secret" }),
-    },
-  } */
+ 
 });
